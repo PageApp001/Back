0525affb43e60e2b9dd3539f97f0a551792755d9
JhonGS01 <git409705@gmail.com>
2024-09-25 00:00:17 -0500

    Update add clodinary

--- src/controllers/carousel.controller.ts
+++ src/controllers/carousel.controller.ts
@@ -1,73 +1,106 @@
 import { Request, RequestHandler, Response } from "express";
 import { CarouselService } from "../services/carousel.services";
+import { CloudinaryService } from "../services/cloudinary.service";
 
-export const createCarouselImage: RequestHandler = async (req: Request, res: Response) => {
-    const carouselService = new CarouselService();
-    try {
-        const { link } = req.body;
-        const imagen = req.file ? req.file.filename : null;
-        const carousel = await carouselService.create({ link, imagen  });
-        if (!imagen) {
-            return res.status(400).json({ message: "Image is required" });
-        }
+const cloudinaryService = new CloudinaryService();
 
-        return res.status(201).json({
-            message: "Image created successfully",
-            data: carousel
-        });
-    } catch (error: any) {
-        return res.status(500).json({
-            message: error.message
-        });
+export const createCarouselImage: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const carouselService = new CarouselService();
+  try {
+    const { link } = req.body;
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: "Image is required" });
     }
-}
 
+    // Subir la imagen a Cloudinary
+    const result = await cloudinaryService.uploadImage(file);
 
-export const getCarouselImages: RequestHandler = async (req: Request, res: Response) => {
-    const carouselService = new CarouselService();
-    try {
-        const carousel = await carouselService.get();
-        return res.status(200).json(carousel);
-    } catch (error) {
-        console.error("Error fetching carousel images:", error);
-        return res.status(500).json({ message: "Can't fetch all carousel images." });
+    const carousel = await carouselService.create({
+      imagen: result.secure_url, // Usar la URL de la imagen en Cloudinary
+      link,
+    });
+
+    return res.status(201).json({
+      message: "Image created successfully",
+      data: carousel,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const getCarouselImages: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const carouselService = new CarouselService();
+  try {
+    const carousel = await carouselService.get();
+    return res.status(200).json(carousel);
+  } catch (error) {
+    console.error("Error fetching carousel images:", error);
+    return res
+      .status(500)
+      .json({ message: "Can't fetch all carousel images." });
+  }
+};
+
+export const updateCarouselImage: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+  const carouselService = new CarouselService();
+  try {
+    const existingImage = await carouselService.find(id);
+    const file = req.file;
+    const { link } = req.body;
+
+    let newImageUrl = existingImage.imagen; // Mantener la imagen existente
+
+    // Si hay una nueva imagen, subirla a Cloudinary
+    if (file) {
+      const result = await cloudinaryService.uploadImage(file);
+      newImageUrl = result.secure_url; // Actualizar la URL con la nueva imagen
     }
-}
 
-export const updateCarouselImage: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const carouselService = new CarouselService();
-    try {
-        // Si no se envía una nueva imagen, conservamos la imagen existente
-        const existingImage = await carouselService.find(id);
-        const imagen = existingImage.imagen;
+    const updatedCarousel = await carouselService.update(id, {
+      imagen: newImageUrl,
+      link,
+    });
 
-        const { link } = req.body;
+    return res.status(200).json({
+      message: "Image updated successfully",
+      data: updatedCarousel,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-        const carousel = await carouselService.update(id, { imagen, link });
-        return res.status(200).json({
-            message: "Image updated successfully",
-            data: carousel
-        });
-    } catch (error: any) {
-        return res.status(500).json({
-            message: error.message
-        });
-    }
-}
-
-
-export const deleteCarouselImage: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const carouselService = new CarouselService();
-    try {
-        await carouselService.delete(id);
-        return res.status(200).json({
-            message: "Image deleted successfully"
-        });
-    } catch (error: any) {
-        return res.status(500).json({
-            message: error.message
-        });
-    }
-}
+export const deleteCarouselImage: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+  const carouselService = new CarouselService();
+  try {
+    await carouselService.delete(id);
+    return res.status(200).json({
+      message: "Image deleted successfully",
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
